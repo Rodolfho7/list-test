c1af852745d5cd82fd34b837f67a7ca41ac64709
unknown <rodolfhoazevedo@gmail.com>
2022-05-05 23:31:49 -0300

    [feature] add paths to services and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { ToolbarModule } from './components/toolbar/toolbar.module';
+import { ToolbarModule } from '@components/toolbar/toolbar.module';
 
 @NgModule({
   declarations: [
