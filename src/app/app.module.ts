1335b196ba1dca755a18d6895920d231fba9da23
unknown <rodolfhoazevedo@gmail.com>
2022-05-05 22:53:19 -0300

    [feature] add buttons into toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ToolbarModule } from './components/toolbar/toolbar.module';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
