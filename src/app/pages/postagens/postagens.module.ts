75997c6edfcc4f65d1d8cc174b0991b380735b19
unknown <rodolfhoazevedo@gmail.com>
2022-05-06 11:39:48 -0300

    [feature] created a loading component and add it to postagens page

--- src/app/pages/postagens/postagens.module.ts
+++ src/app/pages/postagens/postagens.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostagensComponent } from './postagens.component';
 import { PostagensRoutingModule } from './postagens-routing.module';
+import { LoadingModule } from '@components/loading/loading.module';
 
 @NgModule({
   declarations: [ PostagensComponent ],
   imports: [
     CommonModule,
-    PostagensRoutingModule
+    PostagensRoutingModule,
+    LoadingModule
   ]
 })
 export class PostagensModule { }
