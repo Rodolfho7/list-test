a12c1c792afe7f6ea8bccf87208636726c695ea9
unknown <rodolfhoazevedo@gmail.com>
2022-05-07 17:28:58 -0300

    [feature] add todo updateChecked method

--- src/app/services/api/todo/todos.service.ts
+++ src/app/services/api/todo/todos.service.ts
@@ -17,15 +17,19 @@ export class TodosService {
     return this.httpClient.get<TodoModel[]>(`${this.url}/todos`);
   }
 
-  createTodo(newPost: NewTodoModel): Observable<any> {
+  createTodo(newPost: NewTodoModel): Observable<TodoModel> {
     return this.httpClient.post<TodoModel>(`${this.url}/todos`, { newPost });
   }
 
-  updateTodo(post: TodoModel): Observable<any> {
+  updateTodo(post: TodoModel): Observable<TodoModel> {
     return this.httpClient.put<TodoModel>(`${this.url}/todos/${post.id}`, { post });
   }
 
   removeTodo(postId: number): Observable<void> {
     return this.httpClient.delete<void>(`${this.url}/todos/${postId}`);
+  }
+
+  updateChecked(todoId: number, completed: boolean): Observable<TodoModel> {
+    return this.httpClient.patch<TodoModel>(`${this.url}/todos/${todoId}`, { completed });
   }
 }
