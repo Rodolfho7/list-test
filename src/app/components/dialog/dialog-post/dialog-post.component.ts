b59b2047adaeee41545c9a3dd3283d9519f7f91f
unknown <rodolfhoazevedo@gmail.com>
2022-05-07 13:31:13 -0300

    [feature] dialog post working

--- src/app/components/dialog/dialog-post/dialog-post.component.ts
+++ src/app/components/dialog/dialog-post/dialog-post.component.ts
@@ -1,5 +1,9 @@
 import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PostsService } from '@services/api/posts.service';
+import { Observable, catchError, of } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dialog-post',
@@ -8,12 +12,50 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class DialogPostComponent {
 
+  post: FormGroup;
+  loading = false;
+  postObservable: Observable<any> | undefined;
+
   constructor(
     public dialogRef: MatDialogRef<DialogPostComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) { }
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private fb: FormBuilder,
+    private postService: PostsService
+  ) {
+    this.post = this.fb.group({
+      title: [this.data?.title, Validators.required],
+      body: [this.data?.body, Validators.required],
+    });
+  }
 
   closeDialog(save: boolean): void {
-    this.dialogRef.close(save);
+    if (!save) {
+      this.dialogRef.close(save);
+      return;
+    }
+
+    this.loading = true;
+
+    let postData = {
+      ...this.data,
+      ...this.post.value
+    };
+
+    if (!this.data?.id) {
+      postData = { ...postData, userId: 1 };
+      this.postObservable = this.postService.createPost(postData);
+    } else {
+      this.postObservable = this.postService.updatePost(postData);
+    }
+
+    this.postObservable.pipe(
+      take(1),
+      tap(() => this.loading = false),
+      tap((response) => this.dialogRef.close(response)),
+      catchError(() => {
+        this.loading = false;
+        return of(null);
+      })
+    ).subscribe();
   }
 }
