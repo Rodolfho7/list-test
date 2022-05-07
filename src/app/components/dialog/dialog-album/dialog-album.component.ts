e48bda070474eeeef23b61ad2354eacd676029ad
unknown <rodolfhoazevedo@gmail.com>
2022-05-07 13:53:15 -0300

    [feature] dialog album working

--- src/app/components/dialog/dialog-album/dialog-album.component.ts
+++ src/app/components/dialog/dialog-album/dialog-album.component.ts
@@ -1,5 +1,9 @@
 import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AlbunsService } from '@services/api/albuns.service';
+import { Observable, catchError, of } from 'rxjs';
+import { tap, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dialog-album',
@@ -8,12 +12,49 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class DialogAlbumComponent {
 
+  album: FormGroup;
+  loading = false;
+  albumObservable: Observable<any> | undefined;
+
   constructor(
     public dialogRef: MatDialogRef<DialogAlbumComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) { }
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private fb: FormBuilder,
+    private albunsService: AlbunsService
+  ) {
+    this.album = this.fb.group({
+      title: [this.data?.title, Validators.required]
+    });
+  }
 
   closeDialog(save: boolean): void {
-    this.dialogRef.close(save);
+    if (!save) {
+      this.dialogRef.close(save);
+      return;
+    }
+
+    this.loading = true;
+
+    let albumData = {
+      ...this.data,
+      ...this.album.value
+    };
+
+    if (!this.data?.id) {
+      albumData = { ...albumData, userId: 1 };
+      this.albumObservable = this.albunsService.createAlbum(albumData);
+    } else {
+      this.albumObservable = this.albunsService.updateAlbum(albumData);
+    }
+
+    this.albumObservable.pipe(
+      take(1),
+      tap(() => this.loading = false),
+      tap((response) => this.dialogRef.close(response)),
+      catchError(() => {
+        this.loading = false;
+        return of(null);
+      })
+    ).subscribe();
   }
 }
